c0a7b76d6f7f4219e3ddf66c96a52f3b683821ac
Daniel <ayanokoujik81@gmail.com>
2024-11-20 14:42:36 +0800

    Chatgpt

--- Decimal to Binary vice versa/Decimal to Binary vice versa.cpp
+++ Decimal to Binary vice versa/Decimal to Binary vice versa.cpp
@@ -2,10 +2,32 @@
 //
 
 #include <iostream>
+using namespace std;
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int Decimal;
+    string Binary;
+
+    cout << "Enter Decimal: ";
+    cin >> Decimal;
+
+    if (Decimal == 0)
+    {
+        Binary = "0";
+    }
+    
+    else
+    {
+        while (Decimal > 0)
+        {
+            Binary = to_string(Decimal % 2) + Binary;
+            Decimal /= 2;
+        }
+    }
+
+    cout << "The Binary is: "<< Binary;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
